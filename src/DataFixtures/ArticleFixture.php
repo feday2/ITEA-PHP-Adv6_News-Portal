f3c20f709944ccae143cf4028c2387c4468f9f64
greeflas <vldmr.kuprienko@gmail.com>
2019-10-31 20:43:52 +0200

    Create Article entity

---
+++ src/DataFixtures/ArticleFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ArticleFixture extends AbstractFixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 15; $i++) {
+            $title = \ucfirst($this->faker->words($this->faker->numberBetween(3, 5), true));
+            $article = new Article($title);
+
+            $description = \ucfirst($this->faker->words($this->faker->numberBetween(4, 8), true));
+            $article
+                ->setDescription($description)
+                ->setImage($this->faker->imageUrl())
+            ;
+
+            $body = '';
+            $sentences = $this->faker->numberBetween(8, 20);
+
+            for ($j = 0; $j < $sentences; $j++) {
+                $body .= '<p>'
+                    . $this->faker->words($this->faker->numberBetween(4, 8), true)
+                    . '</p>'
+                ;
+            }
+
+            $article->setBody($body);
+
+            if ($this->faker->boolean(80)) {
+                $article->publish();
+            }
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
